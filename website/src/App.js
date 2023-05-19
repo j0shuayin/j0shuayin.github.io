3fd7e72a09c4054c71df6a3a09d2414963a111f8
joshuayinucla <joshuayin2005@gmail.com>
2023-05-18 18:37:32 -0700

    Initial commit.

---
+++ website/src/App.js
@@ -0,0 +1,17 @@
+import load from './load.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={load} className="Load" alt="load" />
+        <p>
+          Under Construction...
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
