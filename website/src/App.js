4f10b655d9cab480b8e0c433fb63f3cfe5f1141e
joshuayinucla <joshuayin2005@gmail.com>
2025-04-25 14:13:21 -0700

    Add page routing, Education page

--- website/src/App.js
+++ website/src/App.js
@@ -1,16 +1,36 @@
-// import load from './load.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+
+import Education from './Education.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={load} className="Load" alt="load" /> */}
-        <p>
-          Joshua Yin
-        </p>
-        Aspiring software engineer interested in cloud and AI/ML.
-      </header>
+    <Router>
+      <div className="app-container">
+        <header>
+          <nav>
+            <Link to="/">Home</Link> | <Link to="/education">Education</Link>
+          </nav>
+        </header>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/education" element={<Education />}/>
+        </Routes>
+      </div>
+
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <div className="Home">
+      <p>
+        Joshua Yin
+      </p>
+          Aspiring software engineer interested in cloud and AI/ML.
     </div>
   );
 }
---
+++ website/src/Education.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import logo from './UCLALogo.jpg';
+
+function Education() {
+    return (
+        <div className="education-container">
+            <div className="education-item">
+                <img src={logo} alt="" width="50"/>
+                <div className="education-text">
+                    <h3>M.S. Computer Science</h3>
+                    <p>Expected June 2026</p>
+                </div>
+            </div>
+
+            <div className="education-item">
+                <img src={logo} alt="" width="50"/>
+                <div className="education-text">
+                    <h3>B.S. Computer Science</h3>
+                    <p>June 2025</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Education;
